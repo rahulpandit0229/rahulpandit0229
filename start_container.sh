8597fa88416e998c92e99e1b0c78a0ea6b4913ad
Rahul Pandit <104353901+rahulpandit0229@users.noreply.github.com>
2023-10-20 23:55:12 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,8 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull rahulpandit0229/simple-python-flask-app
+
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 rahulpandit0229/simple-python-flask-app
